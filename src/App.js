2c237f7e2bb4db0ec024e70109ea0ab020324754
Praveen1692 <61341264+Praveen1692@users.noreply.github.com>
2022-10-04 13:14:05 +0530

    Add all my files

--- src/App.js
+++ src/App.js
@@ -1,35 +1,25 @@
 import React from "react";
 import "./App.css";
-import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
-import Main from "./Components/Main";
-import { Link } from 'react-router-dom';
+
+
+import Card from './Components/Card';
+import Raf from "./Components/Raf";
+
+
+
 
 function App() {
   return (
-    <div className="demo-big-content">
-      <Layout>
-        <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">My Portfolio</Link>} scroll>
-          <Navigation>
-            <Link to="/Resume">Resume</Link>
-            <Link to="/Aboutme">About Me</Link>
-         
-            <Link to="/Contact">Contact</Link>
-          </Navigation>
-        </Header>
-        <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">My Portfolio</Link>}>
-          <Navigation>
-            <Link to="/Resume">Resume</Link>
-            <Link to="/Aboutme">About Me</Link>
-            <Link to="/Projects">Projects</Link>
-            <Link to="/Contact">Contact</Link>
-          </Navigation>
-        </Drawer>
-        <Content>
-          <div className="page-content"></div>
-          <Main />
-        </Content>
-      </Layout>
-    </div>
+    <>
+    
+    <Card />
+    
+    
+    
+    
+    
+    </>
+    
   );
 }
 
